4714ff8b2d068beaf7135b0255ddfb4d1c883d86
antlao92 <anthonykimlao@gmail.com>
2023-07-07 06:44:36 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(gifts) {
+const newArray = [];
+  for (let i = 0; i < gifts.length; i++) {
+    newArray.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+  }
+
+  return newArray;
+}
+
+writeCards(gifts);
+
+
+function countDown(number){
+    let i = 0;
+    while (i <= number){
+        console.log(number-i);
+        i++
+    }
+}
